50e5ac80d8a39676618c0709b0b6cf529f7509c7
Daniela Ruiz <daniela@nl-ix.net>
2023-10-08 20:00:40 +0200

    add: utils.js
    Utils module with helper functions. For now, it only contains a method to generate JWT to be able to use the API.

---
+++ utils.js
@@ -0,0 +1,11 @@
+const Jwt = require('jsonwebtoken');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+module.exports = {
+    generateToKen: (userId) => {
+        const token = Jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: process.env.TOKEN_EXPIRATION_HOURS });
+        return { token };
+    }
+}
