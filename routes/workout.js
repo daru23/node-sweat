35eca99162e408277c3d297a34e88060ebb31e08
Daniela Ruiz <daniela@nl-ix.net>
2023-10-08 23:57:59 +0200

    feat: PUT and DELETE methods
    Added routes to update and delete WorkoutSessions.

--- routes/workout.js
+++ routes/workout.js
@@ -90,5 +90,82 @@ module.exports = [
                 payload: workoutSessionValidator.label('WorkoutSession'),
             }
         }
+    },
+    {
+        method: 'PUT',
+        path: '/workouts/{id}',
+        handler: async (request, h) => {
+            try {
+                // Get workout id from the route parameters
+                const { id } = request.params;
+                const updatedWorkoutSessionData = request.payload;
+
+                // Find the workout by Id in the database
+                const updatedWorkout = await WorkoutSession.findByIdAndUpdate(id, updatedWorkoutSessionData, {
+                    new: true, // Return the updated workout
+                });
+
+                // toObject() to be able to validate response
+                const workoutSession = updatedWorkout.toObject();
+
+                return h.response(workoutSession).code(200);
+            } catch (error) {
+                console.error(error);
+                return h.response({ error: 'Internal Server Error' }).code(500);
+            }
+        },
+        config: {
+            auth: 'jwt', // Require JWT authentication
+            tags: ['api', 'workout', 'session', 'id'],
+            description: 'Update a workout session.',
+            response: {
+                schema: workoutSessionValidator.label('WorkoutSession'),
+            },
+            validate: {
+                headers: Joi.object({
+                    authorization: Joi.string()
+                        .required()
+                        .description('JWT Token in the Authorization header'),
+                }).options({allowUnknown: true}),
+                params: Joi.object({
+                    id: Joi.string().required(),
+                }),
+                payload: workoutSessionValidator.label('WorkoutSession'),
+            }
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/workouts/{id}',
+        handler: async (request, h) => {
+            try {
+                // Get workout id from the route parameters
+                const { id } = request.params;
+
+                // Find the workout by Id and removing it
+                await WorkoutSession.findByIdAndRemove(id);
+
+                // Return a success response with status code 204 (No Content)
+                return h.response().code(204);
+            } catch (error) {
+                console.error(error);
+                return h.response({ error: 'Internal Server Error' }).code(500);
+            }
+        },
+        config: {
+            auth: 'jwt', // Require JWT authentication
+            tags: ['api', 'workout', 'session', 'id'],
+            description: 'Delete a workout session.',
+            validate: {
+                headers: Joi.object({
+                    authorization: Joi.string()
+                        .required()
+                        .description('JWT Token in the Authorization header'),
+                }).options({allowUnknown: true}),
+                params: Joi.object({
+                    id: Joi.string().required(),
+                })
+            }
+        }
     }
 ];
